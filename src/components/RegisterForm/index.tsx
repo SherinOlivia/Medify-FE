e5bede5ec035ed7ebdc6694ef0d3085fa541a023
Sherin Olivia <SOChronicle@gmail.com>
2023-12-06 11:52:22 +0700

    chore: created Register Form branch

---
+++ src/components/RegisterForm/index.tsx
@@ -0,0 +1,24 @@
+// import styles from './RegisterForm.module.css'
+// import { RegisterInfo } from '../../types';
+
+// interface Props {
+//     onSubmit: (values: RegisterInfo) => void
+// }
+// const Register = ({ onSubmit }: Props) => {
+
+const Register = () => {
+
+    // const handleRegister = async (values: RegisterInfo) => {
+    //     console.log(`Successfully Registered..!`, values)
+    //     onSubmit(values)
+    //   }
+      
+      return (
+        <>
+            <h1>Hello World</h1>
+        </>
+      )
+ 
+    };
+
+  export default Register
