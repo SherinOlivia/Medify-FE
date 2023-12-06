ac7318fe60a750a395baf9e63e9b14111866a24e
Sherin Olivia <SOChronicle@gmail.com>
2023-12-06 11:57:27 +0700

    chore: created LoginForm branch

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,1 +1,19 @@
 // import styles from './LoginForm.module.css'
+// import { LoginInfo } from '../../types';
+
+const LoginForm = () => {
+
+    // const handleLogin = async (values: LoginInfo) => {
+    //     console.log(`Successfully Logged in..!`, values)
+    //     onSubmit(values)
+    //   }
+      
+      return (
+        <>
+            <h1>Welcome to World</h1>
+        </>
+      )
+ 
+    };
+
+  export default LoginForm
